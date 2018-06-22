220fcb52b9000e6c5510232c6d9f436e40156354
Унгер Олег Владимирович <unger@Aliton.net>
2018-06-22 18:16:34 +0300

    -

--- index.js
+++ index.js
@@ -1,42 +1,95 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    fortune = require('./lib/fortune.js');
 
 
 app.set('port', process.env.PORT || 3000);
 
 app.set('view engine', 'pug');
-app.set('views', './views/pages');
+app.set('views', './views');
 
 app.use(express.static(__dirname + '/public'));
+app.use((req, res, next)=>{
+    res.locals.showTests = app.get('env')!== 'production' && req.query.test === '1';
+    next();
+})
 
-var fortunes = [
-    "Победи свои страхиб иначе они победят тебя",
-    "Рекам нужны истоки",
-    "Не бойся неведомого",
-    "Тебя ждет приятный сюрприз",
-];
 
 app.get('/', (req, res)=>{
     res.render('home', { title: 'Home', message: 'Главная!' })
 });
 
 app.get('/about', (req, res)=>{
-    var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
-    res.render('about', {fortune: randomFortune })
+    res.render('about', {
+        title: 'About',
+        message: 'About',
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    })
 });
 
 app.use((req, res, next)=>{
-    res.status(404);
-    res.render('404', {title: '404', message: '404 not found!'});
+    res.status(404)
+        .render('404', {title: '404', message: '404 not found!'});
 });
 
 app.use((err, req, res, next)=>{
     console.error(err.stack);
-    res.status(500);
-    res.render('500', {title: '500', message: '500 server error'});
+    res.status(500)
+        .render('500', {title: '500', message: '500 server error'});
 });
 
 app.listen(app.get('port'), ()=>
     console.log(`Express запущен на http://localhost ${app.get('port')};`)
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/tours/hood-river', ()=>{
+//     res.render('/tours/hood-river',{
+//         title: 'hood-river'
+//     });
+// });
+// app.get('/tours/request-group-rate', ()=>{
+//     res.render('/tours/request-group-rate',{
+//         title: 'rating'
+//     });
+// });
+//
+// app.get('/headers', (res, req)=>{
+//     res.set('Content-Type', 'text/plain');
+//     var s = '';
+//     for(let name of req.headers){
+//         s+= `${name} : ${req.headers[name]} \n`;
+//
+//     }
+//     res.send(s);
+// })
 
